cdac3ec41f38b2c037aa25301330bd44ee1a31b5
Noah-Hickey <Noah.Hickey@keyin.com>
2026-03-17 14:24:24 -0230

    Added a personal picture to the Home page with styling and a blue border.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,8 +10,16 @@ export default function Home() {
             <h1 className="text-4xl md:text-5x1 font-bold leaning-tight">Hey, I'm Noah!
             <br />
             Welcome to my personal website.
-            
             </h1>
+
+            <div className="flex justify-center mt-6">
+                <div className = "p-[3px] rounded-full bg-gradient-to-tr from-teal-400 to-blue-500">
+                    <img src="/HomePic_NH.JPG" 
+                    alt="Noah Hickey"
+                    className="w-40 h-40 md:w-52 md:h-52 object-cover rounded-full" 
+                    />
+                </div>
+            </div>
 
             <p className="max-w-2x1 text-lg text-slate-600 dark:text-slate-300 leading-8">
                 I'm a janitorial supervisor and a software development graduate. I have a passion for technology, helping people, history, nature, and learning new things. 
